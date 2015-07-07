f13378c7cea402c6843e2121fe2c36f21900a48a
VK <vk@battleship.ru>
2015-07-07 10:22:03 +0400

    Вынесен REF в автогенерацию

--- web/api.v0-1.php
+++ web/api.v0-1.php
@@ -4,6 +4,233 @@ require_once __DIR__.'/../conf/bootstrap.php';
 require_once __DIR__.'/../conf/conf.php';
 
 $app = \App::getInstance();
+$app->REF = function( $url ) use ($app) {
+    $args = $app->matchAll( "/stat", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindStatisticsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/staff", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindStaffUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/persons", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindPersonsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/persons/:id", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindPersonUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/persons/:id/destinations", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindPersonDestinationsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/persons/:id/sources", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindPersonSourcesUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/documents", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindDocumentsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/documents/published", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindPublishedUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/documents/:id", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindDocumentUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/documents/:id/destinations", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindDocumentDestinationsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/documents/:id/sources", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindDocumentSourcesUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/links", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindLinksUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/links/:id", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindLinkUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/forms(/:cohort)(/:year)", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindFormsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/forms/:cohort/:year/:league", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindFormUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/forms/:cohort/:year/:league/pupils", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindFormPupilsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/unions", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindUnionsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/paths/:id/position", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindPathPositionUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/digests", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindDigestsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/digests/:id", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindDigestUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/digests/:id/sources", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindDigestSourcesUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/events", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindEventsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/events/:id", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindEventUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/events/:id/sources", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindEventSourcesUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/cohorts", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindCohortsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/cohorts/:id", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindCohortUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/cohorts/:id/persons", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindCohortPersonsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/cohorts/:id/documents", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindCohortDocumentsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/cohorts/:id/leagues", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindCohortLeaguesUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/cohorts/:id/leagues/:let", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindCohortLeagueUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/cohorts/:id/leagues/:let/persons", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindCohortLeaguePersonsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    $args = $app->matchAll( "/cohorts/:id/leagues/:let/documents", $url );
+    if( $args !== NULL ) {
+        $usecase = $app->USECASES."\\FindCohortLeagueDocumentsUseCase";
+        $usecase = new $usecase();
+        $result = call_user_func_array(array(&$usecase,"execute"),$args);
+        return $app->handleOutput( $result );
+    }
+    throw new \Exception($url["path"]." not found",404);
+};
 $app->CONTROLLER = function() use ($app) {
     $app->get('/',function() use ($app) {
         $file = dirname(__FILE__)."/schemas/School/School.v0-1.wadl";
@@ -1345,6 +1572,6 @@ $app->CONTROLLER = function() use ($app) {
             }
         }
     );
-    $app->throwError(new Exception("Not found",404));
+    $app->throwError(new \Exception("Not found",404));
 };
 $app->locate("CONTROLLER");
